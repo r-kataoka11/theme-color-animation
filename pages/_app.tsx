007b796eff08cf75604601bc146a8fc4f8e8e7b8
Ryuichi Kataoka <r.kataoka.ps.ai@gmail.com>
2023-04-23 01:24:14 +0900

    🔥 不要なものを削除

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import { store } from '@/store'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { Provider } from 'react-redux'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0,maximum-scale=1.0"
+        />
+      </Head>
       <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
