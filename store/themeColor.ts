4c56ea7491402cb60356e0c6e0c080f1a3f594db
Ryuichi Kataoka <r.kataoka.ps.ai@gmail.com>
2023-04-22 21:02:59 +0900

    ✨ テーマカラーに関するStateを追加

---
+++ store/themeColor.ts
@@ -0,0 +1,62 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export interface ThemeColorState {
+  color: string
+}
+
+export const initialState: ThemeColorState = {
+  color: '#FFFFFF',
+}
+
+// エラー時のテーマカラーアニメーション
+export const error = createAsyncThunk(
+  'themeColor/errorStatus',
+  async (_, thunkApi) => {
+    // 初期化する
+    thunkApi.dispatch(themeColorSlice.actions.resetColor())
+
+    const velocity = 15 // 速度
+
+    let color = 255
+    let reverse = false
+    const animation = () => {
+      // アニメーションの終了条件を満たした場合終了する
+      if (color > 255 && reverse === true) {
+        thunkApi.dispatch(themeColorSlice.actions.resetColor())
+        return
+      }
+      // 折り返し地点に来た時数値を反転する
+      if (reverse === false && color < 0) {
+        reverse = true
+        color = 0
+      }
+
+      // 色を16進数にする
+      const hex = color.toString(16).padStart(2, '0').toUpperCase()
+      thunkApi.dispatch(themeColorSlice.actions.changeColor(`#FF${hex}${hex}`))
+
+      // 色を変更する
+      color = reverse ? color + velocity : color - velocity
+      requestAnimationFrame(animation)
+    }
+    animation()
+  }
+)
+
+export const themeColorSlice = createSlice({
+  name: 'themeColor',
+  initialState,
+  reducers: {
+    // テーマカラーをリセットする
+    resetColor: (state) => {
+      state.color = initialState.color
+    },
+    // テーマカラーを変更する
+    changeColor: (state, action: PayloadAction<string>) => {
+      state.color = action.payload
+    },
+  },
+  extraReducers: (builder) => builder.addCase(error.fulfilled, () => {}),
+})
+
+export const themeColorReducer = themeColorSlice.reducer
