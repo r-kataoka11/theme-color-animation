20cf7c3c4f3d54b6b1aee119cf41527263b75173
Ryuichi Kataoka <r.kataoka.ps.ai@gmail.com>
2023-04-22 21:16:09 +0900

    💄 viewport設定

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0,maximum-scale=1.0"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
